136d198ee13755119e1b5e876b897dcebb03f9e8
Ayse <aysendl@gmail.com>
2020-08-08 22:41:17 -0700

    updated the app

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+module.exports =[
+    require("./workoutController"),
+    require("./htmlRoute")
+
+];
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
-var PORT = process.env.PORT || 3001; 
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001; 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
+app.use(express.static("public")); 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
+  useNewUrlParser: true,
+  useFindAndModify: false
+})
+
+app.use(require("./routes"));
+
+
 app.listen(PORT, function() { 
   console.log("App listening on PORT " + PORT); 
 });
